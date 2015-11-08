9d451fb3028f13b8c801d6b91fcc51f87c6a6cab
Francisco Gutierrez <fsalvador23@gmail.com>
2015-11-08 13:26:51 +0100

    removed travis ci by the moment

--- package.js
+++
@@ -1,7 +0,0 @@
-Package.describe({
-    summary: "Dashboard visualization.",
-    name: "franciscogutierrez:dashboard",
-    version: "0.1",
-    git: "https://github.com/FranciscoGutierrez/uncertaintyDashboard.git",
-    debugOnly: true
-});
