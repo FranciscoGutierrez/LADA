7c4af32df433d65ebe9495fac3594e9d400bf9b6
Francisco Gutierrez <fsalvador23@gmail.com>
2015-11-24 10:24:57 +0100

    Fixed aspects widget

--- client/views/aspectswidget/aspectswidget.js
+++ client/views/aspectswidget/aspectswidget.js
@@ -1,40 +1,34 @@
-Template.aspectswidget.rendered = function(){
-  setTimeout(function() {
-    /***
-    * Bar D3 Code
-    ****/
-    var parent = d3.select('.aspects-content');
-    var height  = parent.node().getBoundingClientRect().height*0.75;
-
-    var x = d3.scale.linear()
-    .domain([1, 100])
-    .range([0, height])
-    .clamp(true);
-
-    var dispatch = d3.dispatch("barChange");
-    var bar = d3.select(".aspects-bar")
-    .style("height", height + "px");
-
-    var barTray = bar.append("div")
-    .attr("class", "bar-tray");
-
-    var barHandle = bar.append("div")
-    .attr("class", "bar-handle");
-
-    barHandle.append("div")
-    .attr("class", "bar-handle-icon");
-
-    bar.call(d3.behavior.drag()
-    .on("dragstart", function() {
-      dispatch.barChange(x.invert(d3.mouse(barTray.node())[0]));
-      d3.event.sourceEvent.preventDefault();
-    })
-    .on("drag", function() {
-      dispatch.barChange(x.invert(d3.mouse(barTray.node())[0]));
-    }));
-
-    dispatch.on("barChange.bar", function(value) {
-      barHandle.style("left", x(value) + "px")
+Template.aspectswidget.events({
+  "click .courses-aspect" : function(event, template) {
+    var animationName = "animated pulse";
+    var animationEnd  = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    $(".coursescard-paper").addClass(animationName).one(animationEnd, function() {
+      $(this).removeClass(animationName);
     });
-  },1000);
-};
+  },
+  "click .grades-aspect" : function(event, template) {
+    var animationName = "animated pulse";
+    var animationEnd  = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    $(".gradescard-paper").addClass(animationName).one(animationEnd, function() {
+      $(this).removeClass(animationName);
+    });
+  },
+  "click .history-aspect" : function(event, template) {
+    var animationName = "animated pulse";
+    var animationEnd  = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    $(".historicalcard-paper").addClass(animationName).one(animationEnd, function() {
+      $(this).removeClass(animationName);
+    });
+  }
+});
+
+Template.aspectswidget.helpers({
+  historyOn: function() {
+    return Session.get("hc-toggle");
+  },
+  gradesOn: function() {
+    return Session.get("gc-toggle");
+  }
+});
+
+Template.aspectswidget.rendered = function(){};
