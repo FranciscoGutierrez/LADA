2ec39fe393d6a27acb593c1371fd09be063ab8a7
Francisco Gutierrez <fsalvador23@gmail.com>
2015-11-25 17:24:17 +0100

    Added progress bubble component

--- client/views/riskwidget/riskWidget.js
+++ client/views/riskwidget/riskWidget.js
@@ -1,14 +1,5 @@
-
 Template.riskwidget.helpers({
   risk: function() {
-    var v = Math.round(Session.get("riskValue")*100);
-    $('.risk-knob').val(v).trigger("change");
-    return v;
+    return Math.round(Session.get("riskValue")*100);;
   }
 });
-
-Template.riskwidget.rendered = function(){
-  $(function() {
-    $(".risk-knob").knob();
-  });
-};
