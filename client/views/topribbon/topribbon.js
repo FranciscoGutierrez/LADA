6944d28fda2bbf09cfab5b0ff8295f95c7c6573b
Francisco Gutierrez <fsalvador23@gmail.com>
2015-12-03 18:13:02 +0100

    Added support for student info

--- client/views/topribbon/topribbon.js
+++ client/views/topribbon/topribbon.js
@@ -4,4 +4,14 @@ Template.topribbon.helpers({
   }
 });
 
-Template.topribbon.rendered = function(){};
+Template.topribbon.events({
+  "click .top-ribbon": function (event,template) {
+    if (template.$("iron-icon").attr("icon") != "icons:arrow-drop-up") {
+      template.$(".top-ribbon").animate({height:"240px"}, 200, function() {});
+      template.$("iron-icon").attr("icon","icons:arrow-drop-up");
+    } else {
+      template.$(".top-ribbon").animate({height:"24px"}, 200, function() {});
+      template.$("iron-icon").attr("icon","icons:arrow-drop-down");
+    }
+  }
+});
