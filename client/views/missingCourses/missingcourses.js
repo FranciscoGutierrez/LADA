6bf507f8c30fe3a7589f5e8218c712e18a4e9207
Francisco Gutierrez <fsalvador23@gmail.com>
2015-12-09 12:59:24 +0100

    Fixed Reactive table and stacked bars

--- client/views/missingCourses/missingcourses.js
+++ client/views/missingCourses/missingcourses.js
@@ -2,22 +2,26 @@
 * Template life Cycle (Events)
 */
 Template.missingcourses.events({
-  "click .gc-toggle": function(event,template) {
-    if(template.$(".sc-toggle").attr("checked")){
-      Session.set("sc-toggle",true);
-    } else {
-      Session.set("sc-toggle",false);
-    }
+  "click .mc-toggle": function(event,template) {
+    var grades = {courses: Session.get("courses"), student: Session.get("student")};
+    template.$(".mc-progress").fadeIn();
+    template.$(".cc-nothing-message").text("Loading dataset...");
+    Session.set("showRadioSC", {passed: true, failed: false, other: false});
+
+    Meteor.subscribe("grades", grades, function() {
+      Session.set("mc-toggle",template.$(".mc-toggle").attr("checked"));
+      template.$(".mc-progress").fadeOut();
+      template.$(".cc-nothing-message").text("This Card is Turned Off");
+    });
   },
   "click .mc-radio-passed": function(event,template) {
     Session.set("showRadioSC", {passed: true, failed: false, other: false});
+    template.$(".mc-radio-failed").attr("checked",false);
   },
   "click .mc-radio-failed": function(event,template) {
     Session.set("showRadioSC", {passed: false, failed: true, other: false});
+    template.$(".mc-radio-passed").attr("checked",false);
   },
-  // "click .radio-gray": function(event,template) {
-  //   Session.set("showRadioSC", {passed: false, failed: false, other: true});
-  // },
   "click .sc-course": function(event,template) {
     var parent = $(bvevent.target).parents(".sc-course");
     template.$(".sc-course").css("background","#fafafa");
@@ -30,10 +34,10 @@ Template.missingcourses.events({
 */
 Template.missingcourses.helpers({
   passedCourses: function() {
-    return Grades.find({"student": Session.get("student"), "status": "AP"}).fetch();
+    return Grades.find({"student": Session.get("student"), "status": "AP"}, {sort: {year: 1}}).fetch();
   },
   failedCourses: function() {
-    return Grades.find({"student": Session.get("student"), "status": "RP"}).fetch();
+    return Grades.find({"student": Session.get("student"), "status": "RP"}, {sort: {year: 1}}).fetch();
   },
   showPassed: function() {
     return Session.get("showRadioSC").passed;
@@ -42,12 +46,20 @@ Template.missingcourses.helpers({
     return Session.get("showRadioSC").failed;
   },
   isOn: function() {
-    return Session.get("sc-toggle");
+    return Session.get("mc-toggle");
   },
   student: function() {
     return Session.get("student");
+  },
+  settings: function () {
+    return {
+      rowsPerPage: 10,
+      showFilter: false,
+      showRowCount: false,
+      showNavigationRowsPerPage: false,
+      fields: ['course', 'grade', 'year']
+    };
   }
 });
 
-
-Template.riskwidget.rendered = function () {};
+Template.missingcourses.rendered = function () {};
